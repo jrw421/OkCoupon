5e32216bb9a098eade689536dd4f7806ab30e141
jessica wolvington <jessicawolvington@jessicas-MacBook-Pro.local>
2018-01-17 15:41:03 -0500

    database saving

--- schema.sql
+++ schema.sql
@@ -3,23 +3,23 @@ CREATE DATABASE okc;
 
 USE okc;
 
-CREATE TABLE Users (
+CREATE TABLE users (
   id int NOT NULL AUTO_INCREMENT,
   user_name varchar(50) NOT NULL,
   password varchar(50) NOT NULL,
   PRIMARY KEY (id)
 );
 
-CREATE TABLE Coupons (
+CREATE TABLE coupons (
   id int NOT NULL AUTO_INCREMENT,
   user_id int NOT NULL,
   latitude varchar(50) NOT NULL,
   longitude varchar(50) NOT NULL,
-  imgUrl varchar(50) NOT NULL,
-  title varchar(50) NOT NULL,
+  imgUrl varchar(255) NOT NULL,
+  title varchar(255) NOT NULL,
   price varchar(50) NOT NULL,
   discount varchar(50) NOT NULL,
-  merchant varchar(50) NOT NULL,
+  merchant varchar(255) NOT NULL,
   -- finePrint varchar(50) NOT NULL,
   -- description varchar(50) NOT NULL,
   url varchar(50) NOT NULL,
@@ -27,7 +27,7 @@ CREATE TABLE Coupons (
   pureUrl varchar(50) NOT NULL,
   PRIMARY KEY (id),
   FOREIGN KEY (user_id)
-    REFERENCES Users(id)
+    REFERENCES users(id)
 );
 
 
