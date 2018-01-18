3a5014eb835947dda7a883550d4f722b02f0f76f
Ethan Lipkind <Ethan.lipkind@gmail.com>
2018-01-18 12:22:23 -0500

    zip code error handling and skipping over coupons that user has already saved.

--- server.js
+++ server.js
@@ -11,85 +11,20 @@ const compiler = webpack(webpackConfig);
 app.use(express.static(__dirname + '/www'));
 app.use(bodyParser.json());
 
-// app.use(webpackDevMiddleware(compiler, {
-//   hot: true,
-//   filename: 'bundle.js',
-//   publicPath: '/',
-//   stats: {
-//     colors: true,
-//   },
-//   historyApiFallback: true,
-// }));
-
-//FOR PASSPORT
-// var strategy = new Auth0Strategy({
-//    domain:       'localhost',
-//    clientID:     'your-client-id',
-//    clientSecret: 'your-client-secret',
-//    callbackURL:  '/callback'
-//   },
-//   function(accessToken, refreshToken, extraParams, profile, done) {
-//     return done(null, profile);
-//   }
-// };
-
-//if user_name is found - get all saved coupons
-// app.post('/user', (req, res) => {
-//   console.log('what is the req in server ', req.body.user_name)
-
-//   db.Coupons.findAll({where: {user_name: req.body.user_name, password: req.body.password}})
-//     .then(() => {
-//       // res.redirect('/')
-//       Coupons.findAll({
-//         include: [{
-//           model: Users
-//         }]
-//       })
-//       .then((data) => {
-//         res.send("SUCCESSSSS")
-//       })
-//     })
-//     // .catch(() => {
-//     //   res.redirect('/newUser')
-//     // })
-// })
-//where id is username id
-// db.Coupons.findAll({where: {user_name: req.body.user_name}}) //FIX THIS
-// .then((data) => {
-//   res.status(200).send(data)
-// })  //, saved: 'true'
-
-
-// app.post('/newUser', (req, res) => {
-//   console.log('req for POST ', req.body)
-//   db.Users.create({
-//     user_name: req.body.user_name,
-//     password: req.body.password
-//   })
-//   .then((data) => {
-//     res.status(200).send(data)
-//   })
-//   .catch(() => {
-//     res.send('oops')
-//   })
-// })
-
 //////////////////////////////// ETHAN
 app.post('/signUp', (req, res) => {
   let u = req.body.user_name;
   let p = req.body.password;
-  // console.log('u and p ', u, ' ', p)
   // save a new username/password combo to users table
   db.addUser(u, p, () => {
     // redirect to login page
-    res.status(201).end(); // still need to create /login page in react router.
+    res.status(201).end();
   });
 });
 
 app.post('/login', (req, res) => {
   let u = req.body.user_name;
   let p = req.body.password;
-  // console.log('logging in: ', u, p);
   db.authenticateUser(u, p, (err, data) => {
     // console.log('data ', data[0].id) //cookie
     if (data.length) {
@@ -113,7 +48,7 @@ app.post('/delete', (req, res) => {
   });
 })
 
-app.get('userData', (req, res) => {
+app.get('/userData', (req, res) => {
   let u = req.body.username;
   // get all user's saved coupons:
   db.getSaved(u, () => {
@@ -124,63 +59,45 @@ app.get('userData', (req, res) => {
 //////////////////////////////////////
 
 app.post('/helper', (req, res) => {
+  let savedCoupons = {};
+  db.getSaved(req.body.userID, (err, data) => {
+    if ( err ) {
+      console.error(err);
+    } else {
+      if ( data.length ) {
+        data.forEach(deal => {
+          savedCoupons[deal.imgUrl] = true
+        });
+      }
+    }
+  });
+
   apiHelp.couponHelper(req.body.postal, req.body.filter, (data) => {
-    var newData = []
+    var newData = [];
     data.deals.forEach((eachDeal) => {
-      var newDeal = {
-            latitude: eachDeal.deal.merchant.latitude.toString(),
-            longitude: eachDeal.deal.merchant.longitude.toString(),
-            imgUrl: eachDeal.deal.image_url,
-            title: eachDeal.deal.title,
-            price: JSON.stringify(eachDeal.deal.price),
-            discount: JSON.stringify(eachDeal.deal.discount_percentage),
-            merchant: eachDeal.deal.merchant.name,
-            url: eachDeal.deal.url,
-            pureUrl: eachDeal.deal.untracked_url
-          }
+      // check if coupon is already in user's saved coupons:
+      if ( savedCoupons[eachDeal.deal.image_url] === undefined ) {
+        var newDeal = {
+              latitude: eachDeal.deal.merchant.latitude.toString(),
+              longitude: eachDeal.deal.merchant.longitude.toString(),
+              imgUrl: eachDeal.deal.image_url,
+              title: eachDeal.deal.title,
+              price: JSON.stringify(eachDeal.deal.price),
+              discount: JSON.stringify(eachDeal.deal.discount_percentage),
+              merchant: eachDeal.deal.merchant.name,
+              url: eachDeal.deal.url,
+              pureUrl: eachDeal.deal.untracked_url
+            }
         newData.push(newDeal)
-    })
+      } else {
+        console.log('you already saved this deal. skipping to the next one.');
+      }
+    });
     res.status(200).send(newData)
   });
 });
 
-// app.post('/helper', (req, res) => {
-
-//   apiHelp.couponHelper(req.body.postal, req.body.filter, (data) => {
-//     for(var i = 0; i < data.deals.length; i++) {
-//       // console.log('data.deald[i]: ', data.deals[i]);
-//       var eachDeal = data.deals[i]
-//       db.Coupons.findOrCreate({where: {
-//           latitude: eachDeal.deal.merchant.latitude.toString(),
-//           longitude: eachDeal.deal.merchant.longitude.toString(),
-//           imgUrl: eachDeal.deal.image_url,
-//           title: eachDeal.deal.title,
-//           price: JSON.stringify(eachDeal.deal.price),
-//           discount: JSON.stringify(eachDeal.deal.discount_percentage),
-//           merchant: eachDeal.deal.merchant.name,
-//           url: eachDeal.deal.url,
-//           pureUrl: eachDeal.deal.untracked_url
-//         }
-//       })
-//         .spread((Teams, created) => {
-//           console.log(Teams.get({
-//             plain: true
-//           }))
-//       })
-//     }
-//     res.status(200).send('done!')
-//   });
-// });
-
-// app.get('/arrayCoupons', (req, res) => {
-//   db.Coupons.findAll({where: {saved: 'null'}, limit: 40}).then((data) => {
-//     res.body = data
-//     res.status(200).send(data)
-//   })
-// })
-//
 app.get('/savedCoupons', (req, res) => {
-  console.log('id: ', req.query.userID);
   db.getSaved(req.query.userID, (err, data) => {
     res.status(200).send(data)
   });
@@ -197,28 +114,6 @@ app.get('/saveCount', (req, res) => {
     }
   });
 });
-
-// for(var i = 0; i < data.deals.length; i++) {
-//   // console.log('data.deald[i]: ', data.deals[i]);
-//   var eachDeal = data.deals[i]
-//   db.Coupons.findOrCreate({where: {
-//       latitude: eachDeal.deal.merchant.latitude.toString(),
-//       longitude: eachDeal.deal.merchant.longitude.toString(),
-//       imgUrl: eachDeal.deal.image_url,
-//       title: eachDeal.deal.title,
-//       price: JSON.stringify(eachDeal.deal.price),
-//       discount: JSON.stringify(eachDeal.deal.discount_percentage),
-//       merchant: eachDeal.deal.merchant.name,
-//       url: eachDeal.deal.url,
-//       pureUrl: eachDeal.deal.untracked_url
-//     }
-//   })
-//     .spread((Teams, created) => {
-//       console.log(Teams.get({
-//         plain: true
-//       }))
-//   })
-// }
 
 ////////////////////////////////////////////////////////////////////////// ETHAN
 // instead of storing all items and then updating items on 'yes', only save items on yes
